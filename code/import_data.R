384721073f582564361eb7fd1598dd824dd2e565
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-05-18 22:41:00 -0300

    Uniformiza com os demais scripts de coleta

--- code/import_data.R
+++ code/import_data.R
@@ -4,10 +4,19 @@ library(tidyverse)
 library(here)
 library(spotifyr)
 
-# Sys.setenv(SPOTIFY_CLIENT_ID = 'coloque a sua')
-# Sys.setenv(SPOTIFY_CLIENT_SECRET = 'coloque a sua')
+# Você precisará pegar um id e segredo para seu código aqui: https://developer.spotify.com/my-applications/#!/applications 
+# 
+chaves = read_csv(here::here("code/chaves-do-spotify.csv"), 
+                  col_types = "cc")
+
+Sys.setenv(SPOTIFY_CLIENT_ID = pull(chaves, client_id))
+Sys.setenv(SPOTIFY_CLIENT_SECRET = pull(chaves, client_secret))
 
 access_token <- get_spotify_access_token()
+
+#
+# PRECISA SER RODADO INTERATIVO, linha a linha: 
+#
 
 lua <- get_artist_audio_features('luiz gonzaga')
 lua %>% 
