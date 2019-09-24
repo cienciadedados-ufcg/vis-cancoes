cb09202b803e9f6f76e33cf53129ef2deef11c39
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2019-09-24 10:28:22 -0300

    Código para ETL das faixas de um conjunto de playlists

--- code/import-dados-playlist.R
+++ code/import-dados-playlist.R
@@ -13,11 +13,27 @@ Sys.setenv(SPOTIFY_CLIENT_SECRET = pull(chaves, client_secret))
 # Autentica com a API e pega token para usar os endpoints 
 access_token <- get_spotify_access_token()
 
-# Acessa as músicas de uma playlist específica
-playlist = get_user_playlists("zananeno") %>% 
-    filter(playlist_name == "30 dias em 30 músicas")
 
-tracks = get_playlist_tracks(p)
+features = get_playlist_audio_features(username = "Spotify", 
+                                       playlist_uris = c("37i9dQZF1DWYYeOy9vs7I5",
+                                                         "37i9dQZF1DX6qtBDSfhRrH", 
+                                                         "37i9dQZF1DWZLiXDryu4Fe", 
+                                                         "37i9dQZF1DWVK7iaoNgUk9", 
+                                                         "37i9dQZF1DWVLcZxJO5zyf", 
+                                                         "37i9dQZF1DXdSjVZQzv2tl",
+                                                         "37i9dQZF1DX7IK76Iw6baR",
+                                                         "37i9dQZF1DX6aTaZa0K6VA"))
 
-tracks %>% 
-    write_csv(here::here("data/playlist-30-30.csv"))
+
+f = features %>% 
+    select_if(~ !is.list(.)) %>% 
+    select(-playlist_img, -playlist_owner_name, -playlist_owner_id, 
+           -analysis_url, -is_local, -primary_color, -starts_with("added"), 
+           -track.episode, -track.track, -track.type, 
+           -track.album.total_tracks, -track.album.total_tracks, 
+           -track.album.type, -track.album.uri, -track.album.external_urls.spotify,
+           -video_thumbnail.url, -starts_with("track.external"), -track.album.href,
+           -track.album.album_type)
+
+f %>% 
+    write_csv(here::here("data/playlists-spotify.csv"))
